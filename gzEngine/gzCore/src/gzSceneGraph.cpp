05612c20cd043acfd0f98c1edcbb65d5d4e992f3
Victor Hugo Flores Marquez <35241075+lGaze@users.noreply.github.com>
2019-07-14 23:32:52 -0500

    added SceneGraph base class

---
+++ gzEngine/gzCore/src/gzSceneGraph.cpp
@@ -0,0 +1,14 @@
+/**************************************************************************/
+/* @filename gzSceneGraph.cpp
+/* @author Victor Flores 
+/* @date 2019/07/14
+/* @brief 
+/**************************************************************************/
+
+#include "gzSceneGraph.h"
+
+namespace gzEngineSDK {
+
+
+
+}
