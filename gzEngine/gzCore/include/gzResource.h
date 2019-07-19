1e15ef9d15fe569423d7844a66573328d66dfdcd
Victor Hugo Flores Marquez <35241075+lGaze@users.noreply.github.com>
2019-07-19 16:09:08 -0500

    Added Resource and ResourceManaer base classes

---
+++ gzEngine/gzCore/include/gzResource.h
@@ -0,0 +1,37 @@
+/**************************************************************************/
+/* @filename gzResource.h
+/* @author Victor Flores 
+/* @date 2019/07/19
+/* @brief 
+/**************************************************************************/
+#pragma once
+
+#include "gzPrerequisitesCore.h"
+
+namespace gzEngineSDK {
+class GZ_CORE_EXPORT Resource
+{
+ public:
+   
+  /**
+   * @brief default constructor
+   */
+  Resource() = default;
+	
+  /**
+   * @brief default destructor
+   */
+  ~Resource() = default;
+	
+  /************************************************************************/
+  /* Resource functions                                                   */
+  /************************************************************************/
+	
+  /************************************************************************/
+  /* Member declarations                                                  */
+  /************************************************************************/
+	
+ public:
+   
+ };
+}
---
+++ gzEngine/gzCore/include/gzResourceManager.h
@@ -0,0 +1,37 @@
+/**************************************************************************/
+/* @filename gzResouceManager.h
+/* @author Victor Flores 
+/* @date 2019/07/19
+/* @brief 
+/*************************************************************************/
+#pragma once
+
+#include "gzPrerequisitesCore.h"
+
+namespace gzEngineSDK {
+class GZ_CORE_EXPORT ResourceManager
+{
+ public:
+   
+  /**
+   * @brief default constructor
+   */
+  ResourceManager() = default;
+	
+  /**
+   * @brief default destructor
+   */
+  ~ResourceManager() = default;
+	
+  /************************************************************************/
+  /* ResourceManager functions                                            */
+  /************************************************************************/
+	
+  /************************************************************************/
+  /* Member declarations                                                  */
+  /************************************************************************/
+	
+ public:
+   
+ };
+}
